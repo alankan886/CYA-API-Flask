fed860a2b3eacbd1e8ca2c0f8942043e271cdcec
Alan Kan <alankan2004@gmail.com>
2020-05-24 13:22:10 -0400

    Created Board and BoardList resources

---
+++ cya/resources/board.py
@@ -0,0 +1,17 @@
+from flask_restful import Resource
+
+class Board(Resource):
+    def get(self):
+        pass
+
+    def post(self, name):
+        pass
+
+    def put(self, name):
+        pass
+
+    def delete(self, name):
+        pass
+
+class BoardList(Resource):
+    pass
