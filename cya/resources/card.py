a237cca81f44263f782af9732261d9cb69c924c9
Alan Kan <alankan2004@gmail.com>
2020-05-24 13:22:44 -0400

    Created Card and CardList resources

---
+++ cya/resources/card.py
@@ -0,0 +1,17 @@
+from flask_restful import Resource
+
+class Card(Resource):
+    def get(self, name):
+        pass
+
+    def post(self, name):
+        pass
+
+    def put(self, name):
+        pass
+
+    def delete(self, name):
+        pass
+
+class CardList(Resource):
+    pass
