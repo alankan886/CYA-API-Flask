40cc385e0f5102c0de7f1e11b172bd555eaaa9e0
Alan Kan <alankan2004@gmail.com>
2020-05-25 22:07:18 -0400

    Planned out CardModel structure and methods

--- cya/models/card.py
+++ cya/models/card.py
@@ -1,3 +1,28 @@
+from db import db
 
 class CardModel():
-    pass
+    __tablename__ = 'cards'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    tag = db.Column(db.String(80))
+    last_checked = db.Column(db.Date)
+    next_checked = db.Column(db.Date)
+
+    def __init__(self):
+        pass
+
+    def json(self):
+        pass
+    
+    @classmethod
+    def find_by_name(cls, name):
+        pass
+
+    def save_to_db(self):
+        pass
+
+    def delete_from_db(self):
+        pass
+
+    
