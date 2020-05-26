127a4eb16ff9e7e899f798bcb27b0cda9dcb6a27
Alan Kan <alankan2004@gmail.com>
2020-05-25 21:09:09 -0400

    Connecting SQLAlchemy to the Flask app

--- cya/app.py
+++ cya/app.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from flask_restful import Api, Resource
+from resources.card import Card
+from resources.board import Board
+from resources.user import UserRegister
+from db import db
 
 app = Flask(__name__)
 api = Api(app)
+db.init_app(app)
 
-api.add_resource(Task, '/card/<string:name>')
-api.add_resource(TaskList, '/board')
+api.add_resource(Card, '/card/<string:name>')
+api.add_resource(Board, '/board')
+api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
+ 
