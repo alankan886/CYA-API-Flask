37293ede2afc9b122127674c712665641fe064b1
Alan Kan <alankan2004@gmail.com>
2020-05-25 22:05:56 -0400

    Added Flask app config for SQLAlchemy

--- cya/app.py
+++ cya/app.py
@@ -6,13 +6,16 @@ from resources.user import UserRegister
 from db import db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
-db.init_app(app)
+
 
 api.add_resource(Card, '/card/<string:name>')
 api.add_resource(Board, '/board')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+    db.init_app(app)
     app.run(port=5000, debug=True)
  
