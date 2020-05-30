7acecc0e1a7a02ba5787241a99ac358e0167319e
Alan Kan <alankan2004@gmail.com>
2020-05-30 17:53:11 -0400

    Created create_tables() and added new endpoints

--- cya/app.py
+++ cya/app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_restful import Api, Resource
+from flask_jwt import JWT
 
 from auth import authenticate, identity
-from resources.card import Card
-from resources.board import Board
+from resources.card import Card, CardList
+from resources.board import Board, BoardList
 from resources.user import UserRegister
 from db import db
 
@@ -12,10 +13,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Card, '/card/<string:name>')
-api.add_resource(Board, '/board')
+api.add_resource(CardList, '/cards')
+api.add_resource(Board, '/board/<string:name>')
+api.add_resource(BoardList, '/boards')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
