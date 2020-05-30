0f6bb8e02485af72dc09bb80f0a5c1740007bbc3
Alan Kan <alankan2004@gmail.com>
2020-05-30 18:00:42 -0400

    Implemented Board resource

--- cya/resources/board.py
+++ cya/resources/board.py
@@ -1,17 +1,37 @@
 from flask_restful import Resource
+from models.board import BoardModel
+
 
 class Board(Resource):
-    def get(self):
-        pass
+    def get(self, name):
+        board = BoardModel.find_by_name(name)
+        if board:
+            return board.json()
+        
+        return {'message' : 'Board not found.'}, 404
 
     def post(self, name):
-        pass
+        if BoardModel.find_by_name(name):
+            return {'message' : "A board with name '{}' already exists.".format(name)}, 400
+
+        board = BoardModel(name)
+        try:
+            board.save_to_db()
+        except:
+            return {'message' : 'An error occurred creating the board.'}, 500
+        
+        return board.json(), 201
 
     def put(self, name):
         pass
 
     def delete(self, name):
-        pass
+        board = BoardModel.find_by_name(name)
+        if board:
+            board.delete_from_db()
+        
+        return {'message' : 'Board deleted.'}
 
 class BoardList(Resource):
-    pass
+    def get(self):
+        return {'boards' : list(map(lambda x: x.json(), BoardModel.query.all()))}
