941e0224670a4e07c77a5d620d5a6a931fb5189e
Alan Kan <alankan2004@gmail.com>
2020-06-03 11:31:04 -0400

    Changed db to MySQL and added flask-migrate

--- cya/app.py
+++ cya/app.py
@@ -1,17 +1,22 @@
 from flask import Flask
 from flask_restful import Api, Resource
 from flask_jwt import JWT
+from flask_migrate import Migrate
 
 from auth import authenticate, identity
 from resources.card import Card, CardList
 from resources.board import Board, BoardList
 from resources.user import UserRegister
 from db import db
+import env
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}:{}@localhost:3306/{}'.format(env.username, env.password, 'cyaDB')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
+db.init_app(app)
+migrate = Migrate(app, db)
+
 
 @app.before_first_request
 def create_tables():
@@ -26,6 +31,5 @@ api.add_resource(BoardList, '/boards')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
-    db.init_app(app)
     app.run(port=5000, debug=True)
  
