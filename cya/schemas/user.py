f783b1836cd0342db390b20513da5ef4ce534132
Alan Kan <alankan2004@gmail.com>
2020-06-10 12:30:03 -0400

    Implemented the UserSchema

--- cya/schemas/user.py
+++ cya/schemas/user.py
@@ -1,4 +1,10 @@
 from ma import ma
+from models.user import UserModel
+
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
-    pass
+    class Meta:
+        model = UserModel
+        load_only = ("password",)
+        dump_only = ("id",)
+        load_instance = True
