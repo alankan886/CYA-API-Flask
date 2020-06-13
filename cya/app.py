3e2f618857db6f1f91535bdf826d3607491ee051
Alan Kan <alankan2004@gmail.com>
2020-06-13 16:43:37 -0400

    Added new resources CardsDueToday and CardsDueTodayOnBoard

--- cya/app.py
+++ cya/app.py
@@ -4,7 +4,7 @@ from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from marshmallow import ValidationError
 
-from resources.card import Card, CardList
+from resources.card import Card, CardList, CardsDueToday, CardsDueTodayOnBoard
 from resources.board import Board, BoardList
 from resources.user import UserRegister, User, UserLogin, UserLogout, TokenRefresh
 from db import db
@@ -82,8 +82,12 @@ def check_if_token_in_blacklist(decrypted_token):
 
 api.add_resource(Card, "/<string:username>/<string:board_name>/<string:card_name>")
 api.add_resource(CardList, "/<string:username>/<string:board_name>/cards")
+api.add_resource(CardsDueToday, "/<string:username>/cards/today")
+api.add_resource(CardsDueTodayOnBoard, "/<string:username>/<string:board_name>/cards/today")
+
 api.add_resource(Board, "/<string:username>/<string:board_name>")
 api.add_resource(BoardList, "/<string:username>/boards")
+
 api.add_resource(UserRegister, "/register")
 api.add_resource(User, "/users/<int:user_id>")
 api.add_resource(UserLogin, "/login")
