28c1acbe96e372ecd60acfa586e39847e8812078
Alan Kan <alankan2004@gmail.com>
2020-06-12 20:09:12 -0400

    Added relationship method to link to BoardModel
    backref and cascade are added to ensure changes and deletions are updated on both users table and boards table.

--- cya/models/user.py
+++ cya/models/user.py
@@ -6,7 +6,9 @@ class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
-      
+    
+    boards = db.relationship('BoardModel', backref='user', cascade="all, delete")
+
     @classmethod
     def find_by_username(cls, username: str) -> "UserModel":
         return cls.query.filter_by(username=username).first()
