27868776172e041a797837ffbf9d67edbd758361
Alan Kan <alankan2004@gmail.com>
2020-06-12 20:13:44 -0400

    Modified HTTP methods to tailor towards specific users
    Now each user will only be able to view and access their own boards and cards.

--- cya/resources/card.py
+++ cya/resources/card.py
@@ -3,22 +3,30 @@ from flask_restful import Resource
 from flask_jwt_extended import jwt_required, fresh_jwt_required
 
 from models.card import CardModel
+from models.board import BoardModel
+from models.user import UserModel
 from schemas.card import CardSchema
+
 
 NAME_ALREADY_EXISTS = "An card with name '{}' already exists."
 CARD_NOT_FOUND = "Card not found."
 CARD_DELETED = "Card deleted."
 CARDS_DELETED = "Cards deleted."
 ERROR_INSERTING = "An error occurred while inserting the item."
+BOARD_NOT_FOUND = "Board not found."
 
 card_schema = CardSchema()
 card_list_schema = CardSchema(many=True)
 
+
 class Card(Resource):
     @classmethod
     @jwt_required
-    def get(cls, name: str):
-        card = CardModel.find_by_name(name)
+    def get(cls, card_name: str, board_name: str, username: str):
+        user = UserModel.find_by_username(username)
+        board = BoardModel.find_by_name(board_name, user.id)
+        card = CardModel.find_by_name(card_name, board.id)
+
         if card:
             return card_schema.dump(card), 200
         
@@ -26,12 +34,19 @@ class Card(Resource):
     
     @classmethod
     @jwt_required
-    def post(cls, name: str):
-        if CardModel.find_by_name(name):
-            return {"message" : NAME_ALREADY_EXISTS.format(name)}, 400
+    def post(cls, card_name: str, board_name: str,username: str):
+        user = UserModel.find_by_username(username)
+        board = BoardModel.find_by_name(board_name, user.id)
+
+        if not board:
+            return {"message": BOARD_NOT_FOUND }
+
+        if CardModel.find_by_name(card_name, board.id):
+            return {"message": NAME_ALREADY_EXISTS.format(card_name)}, 400
         
         card_json = request.get_json()
-        card_json["name"] = name
+        card_json["name"] = card_name
+        card_json["board_id"] = board.id
 
         card = card_schema.load(card_json)
 
@@ -44,29 +59,48 @@ class Card(Resource):
 
     @classmethod
     @fresh_jwt_required
-    def put(cls, name: str):
+    def put(cls, card_name: str, board_name: str, username: str):
+        user = UserModel.find_by_username(username)
+        board = BoardModel.find_by_name(board_name, user.id)
+
         card_json = request.get_json()
-        card = CardModel.find_by_name(name)
+        card = CardModel.find_by_name(card_name, board.id)
 
         if card:
-            if 'tag' in card_json:
-                card.tag = card_json['tag']
-            if 'last_checked' in card_json:
-                card.last_checked = card_json['last_checked']
-            if 'next_check' in card_json:
-                card.next_check = card_json['next_check']
+            name_already_exists = CardModel.find_by_name(card_json["name"], board.id) != None
+
+            if name_already_exists:
+                return {"message": NAME_ALREADY_EXISTS.format(card_json["name"])}, 400
+
+            if "name" in card_json:
+                card.name = card_json["name"]
+            
+            if "tag" in card_json:
+                card.tag = card_json["tag"]
+            
+            if "last_checked" in card_json:
+                card.last_checked = card_json["last_checked"]
+            
+            if "next_check" in card_json:
+                card.next_check = card_json["next_check"]            
         else:
-            card_json["name"] = name
+            card_json["name"] = card_name
             card = card_schema.load(card_json)
         
-        card.save_to_db()
-
+        try:
+            card.save_to_db()
+        except:
+            return {"message" : ERROR_INSERTING}, 500
+        
         return card_schema.dump(card), 200
 
     @classmethod
     @fresh_jwt_required
-    def delete(cls, name: str):
-        card = CardModel.find_by_name(name)
+    def delete(cls,  card_name: str, board_name: str, username: str):
+        user = UserModel.find_by_username(username)
+        board = BoardModel.find_by_name(board_name, user.id)
+        card = CardModel.find_by_name(card_name, board.id)
+
         if card:
             card.delete_from_db()
             return {"message" : CARD_DELETED}, 200
@@ -77,13 +111,19 @@ class Card(Resource):
 class CardList(Resource):
     @classmethod
     @jwt_required
-    def get(cls):
-        return {'cards' : card_list_schema.dump(CardModel.find_all())}, 200
+    def get(cls, board_name: str, username: str):
+        user = UserModel.find_by_username(username)
+        board = BoardModel.find_by_name(board_name, user.id)
+
+        return {"cards" : card_list_schema.dump(CardModel.find_all(board.id))}, 200
     
     @classmethod
     @fresh_jwt_required
-    def delete(cls):
-        for card in CardModel.find_all():
+    def delete(cls, board_name: str, username: str):
+        user = UserModel.find_by_username(username)
+        board = BoardModel.find_by_name(board_name, user.id)
+
+        for card in CardModel.find_all(board.id):
             card.delete_from_db()
 
         return {"message" : CARDS_DELETED}
