1af986fd7d9d52ec945d39f465d199e02bf0ce31
Alan Kan <alankan2004@gmail.com>
2020-06-12 20:03:17 -0400

    Added new column, specified relationship and modified methods
    user_id column is now added to the BoardModel, backref and cascade is added to the cards relationship to keep the two tables updated, and find_ methods now uses user_id for its queries.

--- cya/models/board.py
+++ cya/models/board.py
@@ -8,15 +8,17 @@ class BoardModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
 
-    cards = db.relationship('CardModel')
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+
+    cards = db.relationship('CardModel', backref='board', cascade="all, delete")
 
     @classmethod
-    def find_by_name(cls, name: str) -> 'BoardModel':
-        return cls.query.filter_by(name=name).first()
-
+    def find_by_name(cls, name: str, user_id: int) -> 'BoardModel':
+        return cls.query.filter_by(name=name, user_id=user_id).first()
+    
     @classmethod
-    def find_all(cls) -> List['BoardModel']:
-        return cls.query.all()
+    def find_all(cls, user_id: int) -> List['BoardModel']:
+        return cls.query.filter_by(user_id=user_id).all()
 
     def save_to_db(self) -> None:
         db.session.add(self)
