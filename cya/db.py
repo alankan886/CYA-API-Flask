6f2a2467a1b79520dbb1b9a1077a822be31de66e
Alan Kan <alankan2004@gmail.com>
2020-06-22 21:25:58 -0400

    Added pooling for ClearDB connection problem

--- cya/db.py
+++ cya/db.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from sqlalchemy.pool import NullPool
+from config import SQLALCHEMY_DATABASE_URI
 
 convention = {
     "ix": "ix_%(column_0_label)s",
@@ -8,6 +10,11 @@ convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
     "pk": "pk_%(table_name)s"
 }
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URI,
+    poolclass=NullPool
+)
 
 metadata = MetaData(naming_convention=convention)
 db = SQLAlchemy(metadata=metadata)
