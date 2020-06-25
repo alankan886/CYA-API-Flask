f2c78269de0943108e596dbf83bc44b03341ada1
Alan Kan <alankan2004@gmail.com>
2020-06-25 18:18:22 -0400

    Moved more configs from app.py

--- cya/config.py
+++ cya/config.py
@@ -1,7 +1,14 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('CLEARDB_DATABASE_URL')
 SECRET_KEY = os.environ.get('SECRET_KEY')
+PROPAGATE_EXCEPTIONS = True
+
+SQLALCHEMY_DATABASE_URI = os.environ.get('CLEARDB_DATABASE_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-#DEBUG = os.environ.get('DEBUG')
-#ENV = os.environ.get('ENV')
+
+JWT_BLACKLIST_ENABLED = True
+JWT_BLACKLIST_TOKEN_CHECKS = [
+    "access",
+    "refresh",
+]
+JWT_ACCESS_TOKEN_EXPIRES = 1800
