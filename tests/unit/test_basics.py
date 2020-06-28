07a77d1e0352c6bdf3296d602fda29b18da05067
Alan Kan <alankan2004@gmail.com>
2020-06-28 19:39:03 -0400

    Made folders into packages and added basic unit testing
    The basic unit testing tests app_exists and app_is_testing

--- tests/unit/test_basics.py
+++ tests/unit/test_basics.py
@@ -1,7 +1,30 @@
 import unittest
+import os 
+
 from flask import current_app
-from app import db
+from dotenv import load_dotenv, find_dotenv
+
+from app import create_app
+from app.db import db
 
 class BasicsTestCase(unittest.TestCase):
     def setUp(self):
-        self.app = 
+        load_dotenv(find_dotenv())
+
+        config_name = os.environ.get('FLASK_ENV')
+        print(config_name)
+        self.app = create_app(config_name)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+    
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+    
+    def test_app_exists(self):
+        self.assertFalse(current_app is None)
+    
+    def test_app_is_testing(self):
+        self.assertTrue(current_app.config["TESTING"])
