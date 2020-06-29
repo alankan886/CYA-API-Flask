e38d4fae6dd11c8548561a9dc1af2617954effa8
Alan Kan <alankan2004@gmail.com>
2020-06-28 21:34:16 -0400

    Minor changes on the db import
    Since project structure changed, the level of reference is now different.

---
+++ app/models/card.py
@@ -0,0 +1,44 @@
+from typing import List
+from datetime import date
+
+from ..db import db
+
+class CardModel(db.Model):
+    __tablename__ = 'cards'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    tag = db.Column(db.String(80))
+    last_checked = db.Column(db.Date, default=date.today)
+    next_check = db.Column(db.Date, nullable=False)
+
+    board_id = db.Column(db.Integer, db.ForeignKey('boards.id'), nullable=False)
+
+    @classmethod
+    def find_by_name(cls, name: str, board_id: int) -> 'CardModel':
+        return cls.query.filter_by(name=name, board_id=board_id).first()
+    
+    @classmethod
+    def find_all_by_board_id(cls, board_id: int) -> List['CardModel']:
+        return cls.query.filter_by(board_id=board_id).all()
+    
+    @classmethod
+    def find_all_by_date(cls, next_check: date) -> List['CardModel']:
+        return cls.query.filter_by(next_check=next_check).all()
+    
+    @classmethod
+    def find_all_by_date_and_board(cls, next_check: date, board_id: int) -> List['CardModel']:
+        return cls.query.filter_by(next_check=next_check, board_id=board_id).all()
+    
+    @classmethod
+    def calc_next_check(cls):
+        return
+    
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+
