971aef1d5e38d7bca33569ef71a8dca22a63c5bb
Alan Kan <alankan2004@gmail.com>
2020-06-28 21:39:35 -0400

    Minor changes on model imports and card resource
    Since project structure has changed, now the level of reference is different. Also, an error handler is added for board/cards/today when board is not found.

---
+++ app/resources/board.py
@@ -0,0 +1,98 @@
+from flask import request
+from flask_restful import Resource
+from flask_jwt_extended import jwt_required, fresh_jwt_required
+
+from ..models.board import BoardModel
+from ..models.user import UserModel
+from ..schemas.board import BoardSchema
+
+
+NAME_ALREADY_EXISTS = "A board with name '{}' already exists."
+BOARD_NOT_FOUND = "Board not found."
+BOARD_DELETED = "Board deleted."
+ERROR_INSERTING = "An error occurred while inserting the board."
+NO_REQUEST_BODY = "No update due to empty request body."
+
+board_schema = BoardSchema()
+board_list_schema = BoardSchema(many=True)
+
+
+class Board(Resource):
+    @classmethod
+    @jwt_required
+    def get(cls, board_name: str, username: str):
+        user = UserModel.find_by_username(username)
+        board = BoardModel.find_by_name(board_name, user.id)
+        
+        if board:
+            return board_schema.dump(board), 200
+        
+        return {"message" : BOARD_NOT_FOUND}, 404
+
+    @classmethod
+    @jwt_required
+    def post(cls, board_name: str, username: str):
+        user = UserModel.find_by_username(username)
+        board = BoardModel.find_by_name(board_name, user.id)
+        
+        if board:
+            return {"message" : NAME_ALREADY_EXISTS.format(board_name)}, 400
+        
+        board = BoardModel(name=board_name, user_id=user.id)
+
+        try:
+            board.save_to_db()
+        except:
+            return {"message" : ERROR_INSERTING}, 500
+        
+        return board_schema.dump(board), 201
+
+    @classmethod
+    @fresh_jwt_required
+    def put(cls, board_name: str, username: str):
+        user = UserModel.find_by_username(username)
+        board = BoardModel.find_by_name(board_name, user.id)
+
+        board_json = request.get_json()
+
+        if board:
+            if not board_json:
+                return {"message": NO_REQUEST_BODY}, 400
+
+            name_already_exists = BoardModel.find_by_name(board_json["name"], user.id) != None
+
+            if name_already_exists:
+                return {"message": NAME_ALREADY_EXISTS.format(board_json["name"])}, 400
+            
+            if "name" in board_json:
+                board.name = board_json["name"]
+        else:
+            board_json["name"] = board_name
+            board_json["user_id"] = user.id
+
+        try:
+            board.save_to_db()
+        except:
+            return {"message" : ERROR_INSERTING}, 500
+
+        return board_schema.dump(board), 201
+    
+    @classmethod
+    @fresh_jwt_required
+    def delete(cls, board_name: str, username: str):
+        user = UserModel.find_by_username(username)
+        board = BoardModel.find_by_name(board_name, user.id)
+        
+        if board:
+            board.delete_from_db()
+            return {"message" : BOARD_DELETED}, 200
+
+        return {"message": BOARD_NOT_FOUND}, 404
+
+
+class BoardList(Resource):
+    @classmethod
+    @jwt_required
+    def get(cls, username: str):
+        user = UserModel.find_by_username(username)
+        return {'boards' : board_list_schema.dump(BoardModel.find_all(user.id))}, 200
