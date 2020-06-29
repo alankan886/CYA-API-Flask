763f7fe119684bcf153e54cff00b289415534bd6
Alan Kan <alankan2004@gmail.com>
2020-06-28 21:12:07 -0400

    Moved and modified config.py for test setup
    Class structure is applied for different environments, each environment calls different databases. dotenv library is used to force find .env file when doing unit testing, else unittest can't find .env and would os.environ.get() None.

---
+++ config.py
@@ -0,0 +1,46 @@
+import os
+
+from dotenv import load_dotenv, find_dotenv
+
+class Config:
+    load_dotenv(find_dotenv())
+    
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    PROPAGATE_EXCEPTIONS = True
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    JWT_BLACKLIST_ENABLED = True
+    JWT_BLACKLIST_TOKEN_CHECKS = [
+        "access",
+        "refresh",
+    ]
+    JWT_ACCESS_TOKEN_EXPIRES = 1800
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DEV_DATABASE_URI")
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.getenv("TEST_DATABASE_URI")
+    
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
+
+
+config = {
+    "development": DevelopmentConfig,
+    "testing": TestingConfig,
+    "production": ProductionConfig,
+
+    "default": DevelopmentConfig
+}
+    
