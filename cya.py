f10f2b0e57ca774197f1d66e089a81b09492d8f3
Alan Kan <alankan2004@gmail.com>
2020-06-30 19:18:34 -0400

    Moved flask-migrate to app.__init__.py
    I initially have the migration here following Miguel Grinberg's setup, I think the reason he placed it here was for the flask-CLI, aka you can run flask db migrate, but I couldn't understand why it must be here, and I would hate to just have it there without understanding it. So I moved to where makes sense to me.

--- cya.py
+++ cya.py
@@ -1,7 +1,7 @@
 import os
-from app import create_app, db
-from flask_migrate import Migrate
+from app import create_app
+
 
 
 app = create_app(os.environ.get("FLASK_ENV") or "default")
-migrate = Migrate(app, db)
+
