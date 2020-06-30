100b59153cf6e32c0848c5a2e56e3153590cfe20
Alan Kan <alankan2004@gmail.com>
2020-06-30 19:34:40 -0400

    Added Blueprint for JWT and moved code for organization
    Now the create_app function is much cleaner, the style of importing extensions and blueprints under the function is learned from the Flask doc on factories & blueprints.

--- app/__init__.py
+++ app/__init__.py
@@ -1,56 +1,38 @@
-from flask import Flask, Blueprint
-from flask import Flask, jsonify
-from flask_restful import Api, Resource
-from flask_jwt_extended import JWTManager
+from flask import Flask, Blueprint, jsonify
+from flask_restful import Api
 
-from .jwt_error_handle import (
-    expired_token_callback,
-    invalid_token_callback,
-    missing_token_callback,
-    token_not_fresh_callback,
-    revoked_token_callback,
-)
-
-from .db import db
-from .ma import ma
-from .blacklist import BLACKLIST
 from config import config
 
+
 # A Blueprint instance is needed here to pass to the flask-restful Api class
+# Check Flask-RESTful doc for more info
 api_blueprint_setup = Blueprint('setup', __name__)
 api = Api(api_blueprint_setup)
+
 
 def create_app(config_name: str):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
+    from .extensions.ma import ma
     ma.init_app(app)
-    db.init_app(app)
-    jwt = JWTManager(app)
 
-    from .main import main as main_blueprint
+    from .extensions.db import db
+    db.init_app(app)
+    
+    from .extensions.migrate import migrate
+    migrate.init_app(app, db)
+
+    from .extensions.jwt import jwt
+    jwt.init_app(app)
+
+    from .blueprints.main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    from .cya_api import api as api_blueprint
-    app.register_blueprint(api_blueprint)
+    from .blueprints.jwt import jwt as jwt_blueprint
+    app.register_blueprint(jwt_blueprint)
+
     app.register_blueprint(api_blueprint_setup)
-
-    # might need to add another blueprint instance just for Api()
-
-    @app.before_first_request
-    def create_tables():
-        db.create_all()
-    
-    @jwt.token_in_blacklist_loader
-    def check_if_token_in_blacklist(decrypted_token):
-        return decrypted_token["jti"] in BLACKLIST
-
-    # Decorating the functions
-    #expired_token_callback = jwt.expired_token_loader(expired_token_callback)
-    #invalid_token_callback = jwt.invalid_token_loader(invalid_token_callback)
-    #missing_token_callback = jwt.unauthorized_loader(missing_token_callback)
-    #token_not_fresh_callback = jwt.needs_fresh_token_loader(token_not_fresh_callback)
-    #revoked_token_callback = jwt.revoked_token_loader(revoked_token_callback)
 
     return app
