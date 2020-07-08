407b43be2a38f662a2328d5b9cd1d14118fbbf28
Alan Kan <alankan2004@gmail.com>
2020-07-08 17:38:57 -0400

    Added new columns, modified column names and link to CardSMInfoModel

--- app/models/card.py
+++ app/models/card.py
@@ -1,18 +1,24 @@
 from typing import List
 from datetime import date
 
+from sqlalchemy import desc 
+
 from ..extensions.db import db
+
 
 class CardModel(db.Model):
     __tablename__ = 'cards'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     name = db.Column(db.String(80))
     tag = db.Column(db.String(80))
-    last_checked = db.Column(db.Date, default=date.today)
-    next_check = db.Column(db.Date, nullable=False)
+    quality = db.Column(db.Integer, nullable=False)
+    last_review = db.Column(db.Date, default=date.today)
+    next_review = db.Column(db.Date)
 
     board_id = db.Column(db.Integer, db.ForeignKey('boards.id'), nullable=False)
+
+    card_sm_info = db.relationship('CardSMInfoModel', backref='card', cascade="all, delete")
 
     @classmethod
     def find_by_name(cls, name: str, board_id: int) -> 'CardModel':
@@ -31,8 +37,8 @@ class CardModel(db.Model):
         return cls.query.filter_by(next_check=next_check, board_id=board_id).all()
     
     @classmethod
-    def calc_next_check(cls):
-        return
+    def find_next_card_id(cls, board_id: int):
+        return cls.query.filter_by(board_id=board_id).order_by(cls.id.desc()).first()
     
     def save_to_db(self):
         db.session.add(self)
