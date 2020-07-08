a273e22d795d068ea19825e3d49d5948697f8260
Alan Kan <alankan2004@gmail.com>
2020-07-08 17:19:18 -0400

    Added missing imports

--- app/blueprints/main/errors.py
+++ app/blueprints/main/errors.py
@@ -1,5 +1,8 @@
-from . import main
+from flask import jsonify
 from marshmallow import ValidationError
+
+from . import main
+
 
 @main.app_errorhandler(ValidationError)
 def handle_marshmallow_validation(err):
