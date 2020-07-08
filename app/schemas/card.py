c227c9476f70eeba100e4412e08770b9fa0d68f0
Alan Kan <alankan2004@gmail.com>
2020-07-08 17:52:48 -0400

    Added nested schema and modified load_only
    Added nested CardSMInfoSchema to CardSchema.

--- app/schemas/card.py
+++ app/schemas/card.py
@@ -1,11 +1,14 @@
 from ..extensions.ma import ma
 from ..models.card import CardModel
 from ..models.board import BoardModel
+from ..models.card_sm_info import CardSMInfoModel
+from ..schemas.card_sm_info import CardSMInfoSchema
 
 class CardSchema(ma.SQLAlchemyAutoSchema):
+    card_sm_info = ma.Nested(CardSMInfoSchema, many=True)
+
     class Meta:
         model = CardModel
-        load_only = ('board',)
-        dump_only = ('id',)
+        load_only = ('board', 'quality')
         include_fk = True
         load_instance = True
