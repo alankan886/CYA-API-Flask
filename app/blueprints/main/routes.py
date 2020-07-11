aee7d82fa228352290eabed8d24d26202d02d8b3
Alan Kan <alankan2004@gmail.com>
2020-07-10 20:44:19 -0400

    Added new endpoints for CardSMInfo

--- app/blueprints/main/routes.py
+++ app/blueprints/main/routes.py
@@ -2,12 +2,16 @@ from ... import api
 from ...resources.card import Card, CardList, CardsDueToday, CardsDueTodayOnBoard
 from ...resources.board import Board, BoardList
 from ...resources.user import UserRegister, User, UserLogin, UserLogout, TokenRefresh
-
+from ...resources.card_sm_info import CardSMInfo, CardSMInfoId, CardSMInfoList
 
 api.add_resource(Card, "/<string:username>/<string:board_name>/<string:card_name>")
 api.add_resource(CardList, "/<string:username>/<string:board_name>/cards")
 api.add_resource(CardsDueToday, "/<string:username>/cards/today")
 api.add_resource(CardsDueTodayOnBoard, "/<string:username>/<string:board_name>/cards/today")
+
+api.add_resource(CardSMInfo, "/<string:username>/<string:board_name>/<string:card_name>/sm2-info")
+api.add_resource(CardSMInfoId, "/<string:username>/<string:board_name>/<string:card_name>/sm2-info/<int:id>")
+api.add_resource(CardSMInfoList, "/<string:username>/<string:board_name>/<string:card_name>/all-sm2-info")
 
 api.add_resource(Board, "/<string:username>/<string:board_name>")
 api.add_resource(BoardList, "/<string:username>/boards")
@@ -17,3 +21,4 @@ api.add_resource(User, "/users/<int:user_id>")
 api.add_resource(UserLogin, "/login")
 api.add_resource(TokenRefresh, "/refresh")
 api.add_resource(UserLogout, "/logout")
+
