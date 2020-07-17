f8f9faed8e1ac68ade4bc7f8666ff884788a0b51
Alan Kan <alankan2004@gmail.com>
2020-07-16 21:16:57 -0400

    Replaced basic app test in pytest

---
+++ tests/unit/test_app.py
@@ -0,0 +1,11 @@
+from flask import current_app
+
+from .conftest import app
+
+def test_app_exists(app):
+    with app.app_context():
+        assert current_app is not None
+
+def test_app_is_testing(app):
+    with app.app_context():
+        assert current_app.config["TESTING"] == True
