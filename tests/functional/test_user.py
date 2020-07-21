2aafc01158f78bf03395a96009c8df0f28950122
Alan Kan <alankan2004@gmail.com>
2020-07-20 22:09:30 -0400

    Added functional tests for user resource

--- tests/functional/test_user.py
+++ tests/functional/test_user.py
@@ -2,19 +2,16 @@ import json
 
 import pytest
 
-from .conftest import test_client, init_database, auth_tokens
+from ..conftest import (
+    test_client,
+    init_database,
+    auth_tokens
+)
 
-attr_names = "id, username, password"
+from ..test_cases import attr_names, users_info, invalid_users_info, user_post_register_test_info
 
-users = [
-    (1, "testusername1", "testpassword1"),
-    (2, "testusername2", "testpassword2"),
-    (3, "abc", "123")
-]
-
-
-@pytest.mark.parametrize(attr_names, users)
-def test_valid_user_post_register(id, username, password, test_client, init_database):
+@pytest.mark.parametrize(attr_names, user_post_register_test_info)
+def test_valid_user_post_register(id, username, password, boards, test_client, init_database):
     response = test_client.post('/register',
         content_type='application/json',
         json={"username": username, "password": password}
@@ -26,8 +23,20 @@ def test_valid_user_post_register(id, username, password, test_client, init_data
     assert len(response_data) == 1
     assert response_data["message"] == "User created successfully."
 
-@pytest.mark.parametrize(attr_names, users)
-def test_valid_user_post_login(id, username, password, test_client, init_database, auth_tokens):
+@pytest.mark.parametrize(attr_names, users_info)
+def test_invalid_user_post_register(id, username, password, boards, test_client, init_database):
+    response = test_client.post('/register',
+        content_type='application/json',
+        json={'username': username, 'password': password}
+    )
+
+    response_data = json.loads(response.data)
+    
+    assert response.status_code == 400
+    assert response_data['message'] == "A user with the same username already exists."
+
+@pytest.mark.parametrize(attr_names, users_info)
+def test_valid_user_post_login(id, username, password, boards, test_client, init_database, auth_tokens):
     response = test_client.post('/login',
         content_type='application/json',
         json={"username": username, "password": password}
@@ -41,11 +50,20 @@ def test_valid_user_post_login(id, username, password, test_client, init_databas
     assert "access_token" in response_data
     assert "refresh_token" in response_data
 
-    auth_tokens.access_tokens.append(response_data["access_token"])
-    auth_tokens.refresh_tokens.append(response_data["refresh_token"])
+@pytest.mark.parametrize(attr_names, invalid_users_info)
+def test_invalid_user_post_login(id, username, password, boards, test_client, init_database, auth_tokens):
+    response = test_client.post('/login',
+        content_type='application/json',
+        json={"username": username, "password": password}
+    )
 
-@pytest.mark.parametrize(attr_names, users)
-def test_valid_user_post_logout(id, username, password, test_client, init_database, auth_tokens):
+    response_data = json.loads(response.data)
+
+    assert response.status_code == 401
+    assert response_data['message'] == 'Invalid credentials!'
+
+@pytest.mark.parametrize(attr_names, users_info)
+def test_valid_user_post_logout(id, username, password, boards, test_client, init_database, auth_tokens):
     response = test_client.post('/logout',
         headers={"authorization": "Bearer {}".format(auth_tokens.access_tokens[id-1])}
     )
@@ -54,8 +72,8 @@ def test_valid_user_post_logout(id, username, password, test_client, init_databa
     assert response.status_code == 200
     assert response_data["message"] == "User {} successfully logged out.".format(username)
 
-@pytest.mark.parametrize(attr_names, users)
-def test_valid_user_post_refresh(id, username, password, test_client, init_database, auth_tokens):
+@pytest.mark.parametrize(attr_names, users_info)
+def test_valid_user_post_refresh(id, username, password, boards, test_client, init_database, auth_tokens):
     response = test_client.post('/refresh',
         headers={"authorization": "Bearer {}".format(auth_tokens.refresh_tokens[id-1])}
     )
@@ -64,8 +82,8 @@ def test_valid_user_post_refresh(id, username, password, test_client, init_datab
 
     assert response.status_code == 200
 
-@pytest.mark.parametrize(attr_names, users)
-def test_valid_user_get_id(id, username, password, test_client, init_database):
+@pytest.mark.parametrize(attr_names, users_info)
+def test_valid_user_get_by_id(id, username, password, boards, test_client, init_database):
     response = test_client.get('/users/{}'.format(id))
 
     response_data = json.loads(response.data)
@@ -74,11 +92,29 @@ def test_valid_user_get_id(id, username, password, test_client, init_database):
     assert response_data["id"] == id
     assert response_data["username"] == username
 
-@pytest.mark.parametrize(attr_names, users)
-def test_valid_user_delete_id(id, username, password, test_client, init_database):
+@pytest.mark.parametrize(attr_names, invalid_users_info)
+def test_invalid_user_get_by_id(id, username, password, boards, test_client, init_database):
+    response = test_client.get('/users/{}'.format(id))
+
+    response_data = json.loads(response.data)
+
+    assert response.status_code == 404
+    assert response_data['message'] == 'User not found.'
+
+@pytest.mark.parametrize(attr_names, users_info)
+def test_valid_user_delete_by_id(id, username, password, boards, test_client, init_database):
     response = test_client.delete('/users/{}'.format(id))
 
     response_data = json.loads(response.data)
 
     assert response.status_code == 200
     assert response_data["message"] == "User deleted."
+
+@pytest.mark.parametrize(attr_names, invalid_users_info)
+def test_invalid_user_delete_by_id(id, username, password, boards, test_client, init_database):
+    response = test_client.delete('/users/{}'.format(id))
+
+    response_data = json.loads(response.data)
+
+    assert response.status_code == 404
+    assert response_data["message"] == "User not found."
