60fc3c3973d3edf800dfbbec1851c49691f6e4f5
Alan Kan <alankan2004@gmail.com>
2020-07-26 15:56:43 -0400

    Modified and fixed find_ methods
    find_next_card_id had a bug of taking board_id when it shouldn't, the rest was changed to fit the query param 'today'.

--- app/models/card.py
+++ app/models/card.py
@@ -10,7 +10,7 @@ class CardModel(db.Model):
     __tablename__ = 'cards'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=False)
-    name = db.Column(db.String(80))
+    name = db.Column(db.String(80), nullable=False)
     tag = db.Column(db.String(80))
     quality = db.Column(db.Integer, nullable=False)
     last_review = db.Column(db.Date, default=date.today)
@@ -25,20 +25,26 @@ class CardModel(db.Model):
         return cls.query.filter_by(name=name, board_id=board_id).first()
     
     @classmethod
+    def find_all(cls, kwargs) -> List['CardModel']:
+        board_ids = kwargs["board_ids"]
+
+        if len(kwargs) > 1:
+            next_review = kwargs["next_review"]
+            return cls.query.filter(cls.board_id.in_(board_ids), cls.next_review==cls.next_review <= next_review).all()
+        else:
+            return cls.query.filter(cls.board_id.in_(board_ids)).all()
+
+    @classmethod
     def find_all_by_board_id(cls, board_id: int) -> List['CardModel']:
         return cls.query.filter_by(board_id=board_id).all()
     
     @classmethod
-    def find_all_by_date(cls, next_check: date) -> List['CardModel']:
-        return cls.query.filter_by(next_check=next_check).all()
+    def find_all_by_date_and_board(cls, next_review: date, board_id: int) -> List['CardModel']:
+        return cls.query.filter(cls.next_review==cls.next_review <= next_review, cls.board_id==board_id).all()
     
     @classmethod
-    def find_all_by_date_and_board(cls, next_check: date, board_id: int) -> List['CardModel']:
-        return cls.query.filter_by(next_check=next_check, board_id=board_id).all()
-    
-    @classmethod
-    def find_next_card_id(cls, board_id: int):
-        return cls.query.filter_by(board_id=board_id).order_by(cls.id.desc()).first()
+    def find_next_card_id(cls):
+        return cls.query.order_by(cls.id.desc()).first()
     
     def save_to_db(self):
         db.session.add(self)
