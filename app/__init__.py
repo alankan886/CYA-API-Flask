e476b22a7ccf20d36ef0f7ec72d7d80dbd39520a
Alan Kan <alankan2004@gmail.com>
2020-07-26 15:49:21 -0400

    Slight modification on imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,6 @@
 from flask import Flask, Blueprint
 from flask_restful import Api
 
-from config import config
-
 
 # A Blueprint instance is needed here to pass to the flask-restful Api class
 # Check Flask-RESTful doc for more info
@@ -12,27 +10,33 @@ api = Api(api_blueprint_setup)
 
 def create_app(config_name: str):
     app = Flask(__name__)
-    app.config.from_object(config[config_name])
-    config[config_name].init_app(app)
 
-    from .extensions.db import db
-    db.init_app(app)
+    with app.app_context():
+        from config import config
+        app.config.from_object(config[config_name])
+        config[config_name].init_app(app)
 
-    from .extensions.ma import ma
-    ma.init_app(app)
+        from .extensions.db import db
+        db.init_app(app)
 
-    from .extensions.migrate import migrate
-    migrate.init_app(app, db)
+        from .extensions.ma import ma
+        ma.init_app(app)
 
-    from .extensions.jwt import jwt
-    jwt.init_app(app)
+        from .extensions.migrate import migrate
+        migrate.init_app(app, db)
 
-    from .blueprints.main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+        from .extensions.jwt import jwt
+        jwt.init_app(app)
 
-    from .blueprints.jwt import jwt as jwt_blueprint
-    app.register_blueprint(jwt_blueprint)
+        from .extensions.swagger import swagger
+        swagger.init_app(app)
 
-    app.register_blueprint(api_blueprint_setup)
+        from .blueprints.main import main as main_blueprint
+        app.register_blueprint(main_blueprint)
+
+        from .blueprints.jwt import jwt as jwt_blueprint
+        app.register_blueprint(jwt_blueprint)
+
+        app.register_blueprint(api_blueprint_setup)
 
     return app
